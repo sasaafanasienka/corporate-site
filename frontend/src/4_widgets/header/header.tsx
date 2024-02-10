f5887eb67b3a21a6013d36c1e3c85819fcfcd3b7
sasaafanasienka <afanasienka@yandex.by>
2024-02-10 20:42:20 +0300

    upd: remove unused comments

--- frontend/src/4_widgets/header/header.tsx
+++ frontend/src/4_widgets/header/header.tsx
@@ -14,23 +14,6 @@ const links = [
 ];
 
 export function Header() {
-  // const [opened, { toggle }] = useDisclosure(false);
-  // const [active, setActive] = useState(links[0].link);
-
-  // const items = links.map((link) => (
-  //   <a
-  //     key={link.label}
-  //     href={link.link}
-  //     className={classes.link}
-  //     data-active={active === link.link || undefined}
-  //     // onClick={(event) => {
-  //     //   event.preventDefault();
-  //     //   setActive(link.link);
-  //     // }}
-  //   >
-  //     {link.label}
-  //   </a>
-  // ));
 
   return (
     <header className={classes.header}>
